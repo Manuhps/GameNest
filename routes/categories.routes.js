54c072131edcc0889d56d54b2a6c513af3eaac60
VascoLima21 <you@example.com>
2024-04-28 15:06:08 +0100

    Creating User and Product controller files

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
+
 // import controller middleware
 const moviesController = require("../controllers/movies.controller");
+
 router.route('/')
-.get( moviesController.findAll )
-.post( moviesController.bodyValidator, moviesController.create );
+    .get(moviesController.findAll)
+    .post(moviesController.bodyValidator, moviesController.create);
+
 router.route('/:id')
-.get( moviesController.findOne)
-.put( moviesController.bodyValidator, moviesController.update )
-.delete( moviesController.delete );
+    .get(moviesController.findOne)
+    .put(moviesController.bodyValidator, moviesController.update)
+    .delete(moviesController.delete);
+
 router.all('*', (req, res) => {
-res.status(404).json({ message: 'MOVIES: what???' }); //send a predefined error message
+    res.status(404).json({ message: 'MOVIES: what???' }); //send a predefined error message
 })
+
 //export this router
 module.exports = router;
