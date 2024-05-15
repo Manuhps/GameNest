64b277e0ab64b7e849c9a896f256cc04b80e5787
VascoLima21 <you@example.com>
2024-05-15 14:39:15 +0100

    User controller fixes

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config(); // read environment variables from .env file
 const cors = require('cors');       // middleware to enable CORS (Cross-Origin Resource Sharing)
-
 const express = require('express');
-
 const app = express();
+
 app.use(cors()); //enable ALL CORS requests (client requests from other domain)
 
 app.use(express.json()); //enable parsing JSON body data
 
+app.use('/users', require('./routes/users.routes'))
+
+
 app.listen(process.env.PORT, () => 
-    console.log(`Server running at http://${process.env.DB_HOST}:${process.env.DB_PORT}/`)
+    console.log(`Server running at http://${process.env.HOST}:${process.env.PORT}/`)
 );
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,27 +4,23 @@ const router = express.Router();
 // import users controller
 const usersController = require("../controllers/users.controller");
 
-router.route('/users')
+router.route('/')
+    .post(usersController.register)
     .get(usersController.getUsers)
 
-router.route('/users/login')
+router.route('/login')
     .post(usersController.login)
 
-router.route('/user')
-    .post(usersController.register)
 
-router.route('/users/me')
-    .get(usersController.getUser)
+router.route('/me')
+    // .get(usersController.getUser)
     .patch(usersController.editProfile)
 
-router.route('/users')
-    .post(usersController.register)
-
-router.route('/users/:userID')
-    .patch(usersController.banUser)
+router.route('/:userID')
+// .patch(usersController.banUser)
 
 router.all('*', (req, res) => {
-    res.status(404).json({ message: '404 Not Found' }); //send a predefined error message
+     res.status(404).json({ message: '404 Not Found' }); //send a predefined error message
 })
 
 //export this router
