1273775cb75e6450022870bdbd8032c66540a448
Manuhps <92999699+Manuhps@users.noreply.github.com>
2024-05-20 02:25:27 +0100

    Update gameMode controllers

--- controllers/gameMode.controller.js
+++ controllers/gameMode.controller.js
@@ -4,8 +4,59 @@ const gameMode = require('../models/gameMode.model');
 const { verifyAdmin } = require("../middlewares/jwt");
 
 module.exports = {
-    findAllGameMode: async (req, res) => {
+    findAllGameMode : async (req, res) => {
         try {
+            /*
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "No access token provided" });
+            }
+        
+            await verifyAdmin(req, res);
+            */
+        
+            const page = req.query.page ? parseInt(req.query.page) : 0;
+        
+            if (page < 0 || !Number.isInteger(page)) {
+            return res.status(400).send({ message: "Page must be 0 or a positive integer" });
+            }
+        
+            const limit = 5; 
+        
+            const offset = page * limit;
+        
+            const gameMode = await gameMode.findAll({
+                offset: offset,
+                limit: limit
+            });
+
+            const nextPage = `/users?page=${page + 1}`;
+            const prevPage = page > 0 ? `/users?page=${page - 1}` : null;
+
+            const links = [
+                { rel: "createGameMode", href: "/gameMode", method: "POST" },
+                { rel: "deleteGameMode", href: "/gameMode/:gameModeID", method: "DELETE" },
+                { rel: "nextPage", href: nextPage, method: "GET" },
+                { rel: "prevPage", href: prevPage, method: "GET" }
+            ];
+
+            console.log('success');
+            res.status(200).send({gameMode: gameMode, links: links});
+        } catch (err) {
+            res.status(500).send({
+                message: err.message || "Something went wrong. Please try again later."
+            });
+        }   
+    },
+
+    createGameMode : async (req, res) => {
+        /*try {
+            // Validação de requisição
+            
+            if (!req.body.gameMode || typeof req.body.gameMode !== 'string') {
+                return res.status(400).send({
+                    message: "Game mode name must be a non-empty string"
+                });
+            }
 
             if (!req.headers.authorization) {
                 return res.status(401).send({ message: "No access token provided" });
@@ -13,84 +64,71 @@ module.exports = {
 
             await verifyAdmin(req, res);
 
-            const page = req.query.page ? parseInt(req.query.page) : 0;
-
-            if (page < 0 || !Number.isInteger(page)) {
-                return res.status(400).send({ message: "Page must be 0 or a positive integer" });
+            const existingGameMode = await gameMode.findOne({ gameMode: req.body.gameMode }); 
+    
+            // Se o game mode já existir, retorna um erro 409 (Conflito)
+            if (existingGameMode) {
+                return res.status(409).send({
+                    message: "A game mode with that name already exists."
+                });
             }
+            */
+    
 
-            const limit = 5;
+        
+            if (!req.body) {
+                return res.status(400).send({
+                    message: "Genre content can not be empty"
+                });
+            }
+    
+            // Salva o gênero no banco de dados
+            const gameMode = new gameMode({
+                gameModeName: req.body.gameModeName,
+            });
+    
+            
+            try {
+                const data = await gameMode.save();
+                res.status(201).send({
+                    message:"New game mode created with success."
+                });
+            } catch (err) {
+                res.status(500).send({
+                    message: err.message || "Something went wrong. Please try again later."
+                });
+            }
+    },
 
-            const offset = page * limit;
-
-            const gameModes = await gameMode.find().skip(offset).limit(limit);
-
-            res.status(200).send({ gameModes: gameModes });
-        } catch (error) {
+    deleteGameMode : async (req, res) => {
+        try {
+            /*
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: "No access token provided" });
+              }
+    
+            await verifyAdmin(req, res);
+            */
+    
+            let result = await gameMode.destroy({ where: { gameModeID: req.params.gameModeID}})
+            if (result == 1)
+                return res.status(201).send({
+                     msg: `Game Mode deleted successfully.`
+            
+                });
+            else {
+                res
+                    .status(404)
+                    .send({
+                        messsage: "Game Mpde not found",
+                    });
+            }
+        }
+        catch (error) {
             res.status(500).send({
-                message: error.message || "Something went wrong. Please try again later.",
+                message: "Something went wrong. Please try again later.",
                 details: error,
             });
         }
-    },
-
-    createGameMode: async (req, res) => {
-        // Authorization check
-/*
-        if (!req.headers.authorization) {
-            return res.status(401).send({ message: "No access token provided" });
-        }
-
-        await verifyAdmin(req, res);
-*/
-        // Request validation
-        if (!req.body) {
-            return res.status(400).send({
-                message: "GameMode content can not be empty"
-            });
-        }
-
-        // Create a new GameMode
-        const gameModes = new gameMode({
-            gameModeName: req.body.gameModeName,
-        });
-
-        // Save the GameMode in the database
-        try {
-            const datas = await gameModes.save();
-            res.status(201).send({
-                message:"New game mode created with success."
-            });
-        } catch (err) {
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the GameMode."
-            });
-        }
-    },
-
-    deleteGameMode: async (req, res) => {
-        // Authorization check
-        if (!req.headers.authorization) {
-            //return res.status(401).send({ message: "No access token provided" });
-        }
-
-        await verifyAdmin(req, res);
-
-        // Request validation
-        if (!req.body) {
-            return res.status(400).send({
-                message: "GameMode content can not be empty"
-            });
-        }
-
-        // Delete the GameMode from the database
-        try {
-            const data = await gameMode.deleteOne({ _id: req.body.id });
-            res.send(data);
-        } catch (err) {
-            res.status(500).send({
-                message: err.message || "Some error occurred while deleting the GameMode."
-            });
-        }
-    }
+    }   
 }
