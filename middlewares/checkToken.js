10ce2e25f7ce7971b8b8a67f5aad51abeea2fbf1
VascoLima21 <you@example.com>
2024-05-25 17:43:18 +0100

    Adjusting middlewares

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -2,7 +2,7 @@ module.exports = {
     checkToken: async (req, res, next) => {
         // Check if the token was provided
         if (!req.headers.authorization) {
-            res.status(401).send({ message: "No access token provided" });
+            return res.status(401).send({ message: "No access token provided" });
         }
         next()
     }
