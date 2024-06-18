435981bd5b948dfda4cb2feecb711bb6d9cc5544
tiagojpt <40220427@esmad.ipp.pt>
2024-06-18 04:50:01 +0100

    Points System complete

--- Server/controllers/orders.controller.js
+++ Server/controllers/orders.controller.js
@@ -169,9 +169,7 @@ module.exports = {
     updateOrder: async (req, res) => {
         try {
             const userID = res.locals.userID;
-            const { cardName, cardNumber, cardExpiryDate, products } = req.body;
-
-        
+            const { cardName, cardNumber, cardExpiryDate, products, pointsToUse, pointsEarned  } = req.body;
 
             // Validate cardName
             if (cardName && typeof cardName !== 'string') {
@@ -179,13 +177,19 @@ module.exports = {
             }
     
             // Validate cardNumber
-        
+            if (cardNumber && isNaN(cardNumber)) {
+                return res.status(400).send({ message: "Invalid cardNumber format. It must be an integer." });
+            }
     
             // Validate cardExpiryDate
             if (cardExpiryDate && !/^(\d{4})-(\d{2})-(\d{2})$/.test(cardExpiryDate)) {
                 return res.status(400).send({ message: "Invalid cardExpiryDate format. It must be a valid date." });
             }
-    
+
+            if (pointsToUse && (isNaN(pointsToUse) || pointsToUse < 0)) {
+                return res.status(400).send({ message: "Invalid pointsToUse format. It must be a positive integer." });
+            }
+
             // Validate products if provided
             if (products && Array.isArray(products)) {
                 for (const product of products) {
@@ -203,105 +207,134 @@ module.exports = {
             }
 
             // Find the order
-        const order = await Order.findOne({ where: { userID, state: 'cart' } });
-        if (!order) {
-            return res.status(404).send({ message: "Current order not found." });
-        }
-
-        
-
-        // Update order attributes
-        order.cardName = cardName || order.cardName;
-        order.cardNumber = cardNumber || order.cardNumber;
-        order.cardExpiryDate = cardExpiryDate || order.cardExpiryDate;
-
-        if (cardName || cardNumber || cardExpiryDate) {
-            order.state = 'pending';
-            const currentDate = new Date();
-            const deliverDate = new Date(currentDate);
-            deliverDate.setSeconds(deliverDate.getSeconds() + 30); // Adiciona 10 segundos em vez de 14 dias
-            order.deliverDate = deliverDate;
-
-            await order.save();
-            
-            let pointsToAdd = 0;
-            for (const product of products) {
-                const productToUpdate = await Product.findByPk(product.productID);
-                if (productToUpdate) {
-                    // Adjusting stock based on quantity change
-                    productToUpdate.stock -= product.quantity;
-                    await productToUpdate.save();
-                    
-                    // Calculate points to add based on the sale price of the product
-                    pointsToAdd += (product.salePrice * product.quantity) / 2;
-                }
+            const order = await Order.findOne({ where: { userID, state: 'cart' } });
+            if (!order) {
+                return res.status(404).send({ message: "Current order not found." });
             }
 
-            // Schedule automatic state transitions
-            setTimeout(async () => {
-                // Check if order is still in 'pending' state after 2 days
-                const updatedOrder = await Order.findByPk(order.orderID);
-                if (updatedOrder.state === 'pending') {
-                    updatedOrder.state = 'shipping';
-                    await updatedOrder.save();
-                }
-            }, 5 * 1000); // 2 days in milliseconds
+            const user = await User.findByPk(userID);
+            if (!user) {
+                return res.status(404).send({ message: "User not found." });
+            }
 
-            setTimeout(async () => {
-                // Check if order is still in 'shipping' state on deliverDate
-                const updatedOrder = await Order.findByPk(order.orderID);
+        // Validate if user has enough points
+            if (pointsToUse && pointsToUse > user.points) {
+                return res.status(400).send({ message: "Not enough points available." });
+            }
+            
+            let totalOrderValue = 0;
+            const orderProducts = await OrderProduct.findAll({ where: { orderID: order.orderID } });
+            for (const orderProduct of orderProducts) {
+                totalOrderValue += orderProduct.salePrice * orderProduct.quantity;
+            }   
+
+            let discount = 0;
+            if (pointsToUse) {
+                discount = pointsToUse / 2;
+
+            // Validate if discount is greater than the totalOrderValue
+                if (discount > totalOrderValue) {
+                    return res.status(400).send({ message: "Discount cannot be greater than the total order value." });
+                }
+
+                if (!cardName && !cardNumber && !cardExpiryDate && pointsToUse && discount < totalOrderValue) {
+                    return res.status(400).send({ message: "Points are not enough to cover the total order value." });
+                }
+
+                totalOrderValue -= discount;
+                user.points -= pointsToUse;
+            }
+   
+
+            // Update order attributes
+            order.cardName = cardName || order.cardName;
+            order.cardNumber = cardNumber || order.cardNumber;
+            order.cardExpiryDate = cardExpiryDate || order.cardExpiryDate;
+
+            if ((cardName && cardNumber && cardExpiryDate) || pointsToUse || (cardName && cardNumber && cardExpiryDate && pointsToUse)) {
+                order.state = 'pending';
                 const currentDate = new Date();
-                if (updatedOrder.state === 'shipping' && currentDate >= deliverDate) {
-                    updatedOrder.state = 'delivered';
-                    await updatedOrder.save();
-                }
-            }, deliverDate.getTime() - Date.now()); // Time until deliverDate in milliseconds
-        }
+                const deliverDate = new Date(currentDate);
+                deliverDate.setDate(deliverDate.getDate() + 14); // Adiciona 14 dias (2 semanas)
+                order.deliverDate = deliverDate;
 
-        
-        if (products && Array.isArray(products)) {
-            for (const product of products) {
-                const orderProduct = await OrderProduct.findOne({
-                    where: {
-                        orderID: order.orderID,
-                        productID: product.productID
+                await order.save();
+
+                // Update product stock and user points
+                for (const orderProduct of orderProducts) {
+                    const product = await Product.findByPk(orderProduct.productID);
+                    if (product) {
+                        product.stock -= orderProduct.quantity;
+                        await product.save();
                     }
-                });
+                }
+    
+                // Update user points based on order total
+                const pointsEarned = Math.floor(totalOrderValue / 2);
+                user.points += pointsEarned;
 
-                if (orderProduct) {
-                    // If the product already exists in the order, return an error message
-                    return res.status(400).send({ message: `Product with ID ${product.productID} is already in the cart.` });
-                } else {
-                    // Add new product to the order
-                    await OrderProduct.create({
-                        orderID: order.orderID,
-                        productID: product.productID,
-                        quantity: product.quantity,
-                        salePrice: product.salePrice
+                await user.save();
+
+                // Schedule automatic state transitions
+                setTimeout(async () => {
+                    // Check if order is still in 'pending' state after 2 days
+                    const updatedOrder = await Order.findByPk(order.orderID);
+                    if (updatedOrder.state === 'pending') {
+                        updatedOrder.state = 'shipping';
+                        await updatedOrder.save();
+                    }
+                }, 2 * 24 * 60 * 60 * 1000); // 2 days in milliseconds
+
+                setTimeout(async () => {
+                    // Check if order is still in 'shipping' state on deliverDate
+                    const updatedOrder = await Order.findByPk(order.orderID);
+                    const currentDate = new Date();
+                    if (updatedOrder.state === 'shipping' && currentDate >= deliverDate) {
+                        updatedOrder.state = 'delivered';
+                        await updatedOrder.save();
+                    }
+                }, deliverDate.getTime() - Date.now()); // Time until deliverDate in milliseconds
+            } 
+
+            if (products && Array.isArray(products)) {
+                for (const product of products) {
+                    const orderProduct = await OrderProduct.findOne({
+                        where: {
+                            orderID: order.orderID,
+                            productID: product.productID
+                        }
                     });
 
-                  
+                    if (orderProduct) {
+                        // If the product already exists in the order, return an error message
+                        return res.status(400).send({ message: `Product with ID ${product.productID} is already in the cart.` });
+                    } else {
+                        // Add new product to the order
+                        await OrderProduct.create({
+                            orderID: order.orderID,
+                            productID: product.productID,
+                            quantity: product.quantity,
+                            salePrice: product.salePrice
+                        });
+                    }
                 }
             }
-        }
 
-
-        //const user = await User.findByPk(userID);
-        //user.points += pointsToAdd;
-        //await user.save();
-        
-
-        res.status(200).send({ message: "Order updated successfully." });
+            res.status(200).send({
+                message: "Order updated successfully.",
+                totalValue: totalOrderValue,
+                discount: discount,
+                pointsUsed: pointsToUse || 0,
+                pointsEarned: pointsEarned
+            });
     
         } catch (error) {
-            
             console.error("Erro capturado:", error);
             res.status(500).send({
                 message: "Something went wrong. Please try again later",
                 details: error,
-            }
-            )}
-        
+            });
+        }
     },
 
     updateProductQuantity: async (req, res) => {
