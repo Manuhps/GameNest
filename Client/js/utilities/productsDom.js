9cc509d3d24acda648699b538a7235e5876642fa
VascoLima21 <you@example.com>
2024-06-21 16:30:12 +0100

    Slight change to product card

--- Client/js/utilities/productsDom.js
+++ Client/js/utilities/productsDom.js
@@ -11,13 +11,12 @@ function displayProducts(products, pagination, limit) {
         productCards += `
             <div class="col mb-5">
                 <div class="card h-100">
-                    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Popular</div>
                     <img class="card-img-top" src="${product.img}" alt="${product.name}" />
-                    <div class="card-body p-4">
+                    <div class="card-body p-4 text-bg-light p-3">
                         <div class="text-center">
                             <h5 class="fw-bolder">${product.name}</h5>
                             <span id="sale-price">${product.curPrice}</span>
-                            <div class="stars">${stars}</div> <!-- Display stars here -->
+                            <div class="stars">${stars}</div>
                             <div class="text-center">
                                 <a class="btn btn-outline-dark mt-auto" href="./html/products.html?id=${product.productID}">View options</a>
                             </div>
